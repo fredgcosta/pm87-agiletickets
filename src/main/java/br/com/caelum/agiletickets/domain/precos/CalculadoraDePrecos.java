5fcf6dad5a21c68818ba5c8daeb3dfbcfb995994
Frederico Garcia Costa <fredgcosta@gmail.com>
2014-11-05 12:44:26 -0200

    Refatorando CalculadoraDePrecos

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -5,42 +5,17 @@ import java.math.BigDecimal;
 import br.com.caelum.agiletickets.models.Sessao;
 import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 
-public class CalculadoraDePrecos {
+public class CalculadoraDePrecos{
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
-		BigDecimal preco;
-		
-		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			} else {
-				preco = sessao.getPreco();
-			}
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-			} else {
-				preco = sessao.getPreco();
-			}
-			
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			}
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-			} else {
-				preco = sessao.getPreco();
-			}
+		TipoDeEspetaculo tipoDeEspetaculo = sessao.getEspetaculo().getTipo();
 
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			}
-		}  else {
-			preco = sessao.getPreco();
-		} 
+		CalculoDeAcrescimo calculoDeAcrescimo = tipoDeEspetaculo.getCalculoAcrescimo();
 
-		return preco.multiply(BigDecimal.valueOf(quantidade));
+		BigDecimal precoUnitario = calculoDeAcrescimo.aplica(sessao);
+
+		BigDecimal precoTotal = precoUnitario.multiply(BigDecimal.valueOf(quantidade));
+
+		return precoTotal;
 	}
-
 }
---
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculoParaEspetaculoSemAcrescimo.java
@@ -0,0 +1,13 @@
+package br.com.caelum.agiletickets.domain.precos;
+
+import java.math.BigDecimal;
+
+import br.com.caelum.agiletickets.domain.precos.CalculoDeAcrescimo;
+import br.com.caelum.agiletickets.models.Sessao;
+
+public class CalculoParaEspetaculoSemAcrescimo implements CalculoDeAcrescimo {
+	@Override
+	public BigDecimal aplica(Sessao sessao) {
+		return sessao.getPreco();
+	}
+}
